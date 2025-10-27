05b5ca0e8b8dc207d2e1a4297aca2cddfba44748
Utkarsh Singh Rawat <utkarsh.srawat2003@gmail.com>
2025-10-27 22:09:17 +0530

    Gemini modal change from 1.5 to 2.5

--- actions/home.js
+++ actions/home.js
@@ -85,7 +85,7 @@ export async function processImageSearch(file) {
 
 
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
     
 
